f554ee432420f17aa3a907a9d4960442e3c985d9
jsoares-sys <jjsoares@minha.fag.edu.br>
2025-03-02 11:48:13 -0300

    Alteração do arquivo inclusão de um novo método

--- hello.java
+++ hello.java
@@ -2,4 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
+    }
 }
